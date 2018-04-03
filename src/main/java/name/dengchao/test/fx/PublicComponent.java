8af22edf642d09d237b069e39d1dcd4e6f7d9996
dengchao <dengchao@hujiang.com>
2018-04-04 00:57:53 +0800

    support display json.

--- src/main/java/name/dengchao/test/fx/PublicComponent.java
+++ src/main/java/name/dengchao/test/fx/PublicComponent.java
@@ -1,9 +1,13 @@
 package name.dengchao.test.fx;
 
+import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import name.dengchao.test.fx.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PublicComponent {
@@ -12,6 +16,8 @@ public class PublicComponent {
     private static TextField shade;
     private static Stage primaryStage;
     private static ListView<Plugin> listView;
+
+    private static List<Node> displayNodes = new ArrayList<>();
 
     public static TextField getTextField() {
         return textField;
@@ -43,5 +49,13 @@ public class PublicComponent {
 
     public static void setListView(ListView<Plugin> listView) {
         PublicComponent.listView = listView;
+    }
+
+    public static List<Node> getDisplayNodes() {
+        return displayNodes;
+    }
+
+    public static void setDisplayNodes(List<Node> displayNodes) {
+        PublicComponent.displayNodes = displayNodes;
     }
 }
