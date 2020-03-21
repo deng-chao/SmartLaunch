8acca31d967b2a45e64a784a18b3ec56aafce203
dengchao02 <dengchao02@qutoutiao.net>
2020-03-21 14:09:39 +0800

    resolve blurred text

--- smartlaunch-ui/src/main/java/net/smartlaunch/plugin/hotkey/handler/ListViewCellFactory.java
+++ smartlaunch-ui/src/main/java/net/smartlaunch/plugin/hotkey/handler/ListViewCellFactory.java
@@ -2,6 +2,7 @@ package net.smartlaunch.plugin.hotkey.handler;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
@@ -10,6 +11,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontSmoothingType;
 import javafx.scene.text.Text;
 import lombok.extern.slf4j.Slf4j;
 import net.smartlaunch.base.plugin.Plugin;
@@ -48,11 +50,13 @@ public class ListViewCellFactory extends ListCell<Plugin> {
             desc.setFont(Font.font(20));
             desc.setText(" - " + item.getSummary());
             desc.setFill(Color.WHITE);
+            desc.setFontSmoothingType(FontSmoothingType.LCD);
 
             Text name = new Text();
             name.setFont(Font.font(20));
             name.setText(item.getName());
             name.setFill(Color.WHITE);
+            name.setFontSmoothingType(FontSmoothingType.LCD);
 
             VBox vBox = new VBox();
             HBox main = new HBox();
@@ -63,6 +67,8 @@ public class ListViewCellFactory extends ListCell<Plugin> {
             path.setText(item.getPath() == null ? "unknown" : item.getPath());
             path.setFont(Font.font(12));
             path.setFill(Color.WHITE);
+            path.setTextOrigin(VPos.TOP);
+            path.setFontSmoothingType(FontSmoothingType.LCD);
             vBox.getChildren().add(path);
             vBox.setPadding(new Insets(6, 0, 0, 10));
 
